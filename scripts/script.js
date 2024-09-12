bdb2cb1f454880b9faa8e37422f57776f982d7e2
Subhajit Roy <subhajit0061@gmail.com>
2024-09-12 13:30:46 +0530

    Show multiple cards using JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Services
 class ServicesCardItem {
     constructor(image, title) {
         this.image = image;
@@ -14,7 +15,7 @@ const servicesList = [
     new ServicesCardItem("./assets/icons/services_4.png", "Travel Guide"),
 ];
 
-// Elements
+// Element
 const servicesBody = document.querySelector(".services-body");
 
 // Add services into html
@@ -30,4 +31,104 @@ servicesList.forEach((service) => {
                         <button type="submit">click here</button>`;
 
     servicesBody.appendChild(section);
+});
+
+// Travel Packages
+class TravelPackages {
+    constructor(
+        img,
+        price,
+        days,
+        person,
+        title,
+        rating,
+        details = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa"
+    ) {
+        this.img = img;
+        this.price = price;
+        this.days = days;
+        this.person = person;
+        this.title = title;
+        this.rating = rating;
+        this.details = details;
+    }
+}
+
+const packagesList = [
+    new TravelPackages(
+        "./assets/images/packages_img1.png",
+        250,
+        4,
+        2,
+        "Bali",
+        5
+    ),
+    new TravelPackages(
+        "./assets/images/packages_img2.png",
+        180,
+        5,
+        2,
+        "Thailand",
+        5
+    ),
+    new TravelPackages(
+        "./assets/images/packages_img3.png",
+        350,
+        6,
+        2,
+        "Paris",
+        5
+    ),
+];
+
+const packagesBodyElement = document.querySelector(".travel-packages-body");
+
+packagesList.forEach((package) => {
+    const card = document.createElement("section");
+    card.classList.add("packages-card");
+
+    let ratingHTML = "";
+
+    for (let i = 1; i <= package.rating; i++) {
+        ratingHTML += `<img src="./assets/icons/rating_star.png" />`;
+    }
+
+    card.innerHTML = `<img src="${package.img}" alt="" />
+                        <!-- CARD INFO -->
+                        <section class="packages-info">
+                            <div class="packages-info-content">
+                                <img src="./assets/icons/dollar.png" alt="" />
+                                <p>${package.price}$</p>
+                            </div>
+                            <div class="packages-info-content">
+                                <img src="./assets/icons/calendar.png" alt="" />
+                                <p>${package.days}days</p>
+                            </div>
+                            <div class="packages-info-content">
+                                <img src="./assets/icons/person.png" alt="" />
+                                <p>${package.person}person</p>
+                            </div>
+                        </section>
+
+                        <!-- CARD BODY -->
+                        <section class="packages-card-body">
+                            <section class="packages-title-rating">
+                                <h3 class="packages-title">${package.title}</h3>
+                                <section class="packages-rating">${ratingHTML}</section>
+                            </section>
+
+                            <p>${package.details}</p>
+
+                            <!-- CARD BUTTONS -->
+                            <section class="packages-card-buttons-section">
+                                <button class="packages-card-buttons">
+                                    Book Now
+                                </button>
+                                <button class="packages-card-buttons">
+                                    View Details
+                                </button>
+                            </section>
+                        </section>`;
+
+    packagesBodyElement.appendChild(card);
 });
